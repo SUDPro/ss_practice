f060d0cb489ff66b9763f138889df0594bc1a184
SUDPro <stepan.kurin@yandex.ru>
2020-02-01 17:28:51 +0300

    rewrite chat v-1 app

---
+++ src/main/java/com/simbirsoft/models/User.java
@@ -0,0 +1,29 @@
+package com.simbirsoft.models;
+
+public class User {
+    private Long id;
+    private String login;
+
+    public User() {
+    }
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
